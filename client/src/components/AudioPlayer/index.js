af9a059ecde070a38d8b66af9549bca859fec0e1
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-08-03 08:25:52 -0500

    basic play/pause working: need to figure out how to individualize the plays

---
+++ client/src/components/AudioPlayer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AudioPlayer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AudioPlayer
--- client/src/components/LoopList/index.js
+++ client/src/components/LoopList/index.js
@@ -3,11 +3,13 @@ import bofImg from "../../assets/images/bof.jpg"
 import crybabyImg from "../../assets/images/CryBaby.jpg"
 import gorillaheroImg from "../../assets/images/gorillaHero.jpg"
 import playIcon from '../../assets/icons/play-outline.svg'
+import pauseIcon from '../../assets/icons/pause-outline.svg'
 import bofAudio from '../../assets/audio/bofPreview.mp3'
 import ghAudio from '../../assets/audio/bofPreview.mp3'
 import cryBabyAudio from '../../assets/audio/watermelonPreview.mp3'
 
 function LoopList(){
+    const audioPlayer = document.querySelector('.audio-player')
 
     const [currentPack, setCurrentPack] = useState('')
     const [isPlaying, setIsPlaying] = useState(false)
@@ -38,35 +40,58 @@ function LoopList(){
         },
     ]
 
-    const start = (audio) => {
-        let loopAudio = new Audio(audio)
-        console.log(loopAudio)
-
-        if(!isPlaying) {
-            loopAudio.pause()
-            setIsPlaying(true)
-            loopAudio.play()
-        } else {
-            setIsPlaying(false)
-            delete Audio(audio)
-            loopAudio.pause()
-        }
+    const setPack = (audio) => {
+        setCurrentPack(audio)
+ 
     }
+
+    const startAudio = (audio) => {
+        // clear audio player src before each switch
+        audioPlayer.setAttribute('src', "")
+
+        // Set audio player = currentPack
+        audioPlayer.setAttribute('src', currentPack)
+        // Play audio
+        audioPlayer.play()
+
+        setIsPlaying(true)
+    }
+    const stopAudio = (audio) => {
+      
+        // Pause audio
+        audioPlayer.pause()
+
+        setIsPlaying(false)
+    }
+
+
     return (
         <div className="grid">
+            <audio className="audio-player">
+                
+            </audio>
             {loopPacks.map((pack) => (
-                <div onMouseEnter={() => setCurrentPack(pack.audio)} className="card">
+                <div onMouseEnter={() => setPack(pack.audio)} className="card">
                     <div className="img-container">
                         <img
                             src={pack.img}
                             alt='Yaaya'
                             className="pack-img"key={pack.name}
                         />
+                        {!isPlaying ? (
+                          <img
+                            src={playIcon}
+                            className="play-btn"
+                            onClick={() => startAudio()}
+                           />  
+                        ) : (
                             <img
-                        src={playIcon}
-                        className="play-btn"
-                        onClick={() => start(currentPack)}
-                    />
+                            src={pauseIcon}
+                            className="pause-btn"
+                            onClick={() => stopAudio()}
+                           />  
+                        )}
+                        
                     </div>
                     <h2 className="section-header beat-header">{pack.name}</h2>
                     <h3 className="beat-details">Genre: {pack.genre}</h3>
