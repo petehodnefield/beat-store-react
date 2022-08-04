34064a762b9364c9339b15bd587dc971d4fb55d5
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-08-04 09:14:18 -0500

    added color pallet

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,32 +4,27 @@ import instagram from '../../assets/icons/logo-instagram.svg'
 import tiktok from '../../assets/icons/logo-tiktok.svg'
 
 function Footer() {
+
+    const getDate = () => {
+        return new Date().toLocaleDateString('en-us', {year: "numeric"})
+    }
     return (
         <footer>
-            <h2 className="section-header">Footer</h2>
-            <ul className="icon-list">
-                <a href="https://www.instagram.com/mongamonga_" target="_blank">
-                    <img
-                        src={instagram}
-                        className="icon filter-icon"
-                        alt="Instagram logo"
-                    />
-                </a>
-                <a href="https://www.youtube.com/channel/UC-MaHf-WV-C6yu99T61pk7A" target="_blank">
-                    <img
-                        src={youtube}
-                        className="icon filter-icon"
-                        alt="YouTube logo"
-                    />
-                </a>
-                <a href="https://www.tiktok.com/@mongamonga_?lang=en" target="_blank">
-                    <img
-                        src={tiktok}
-                        className="icon filter-icon"
-                        alt="TikTok logo"
-                    />
-                </a>
-            </ul>
+         
+           <div className="social">
+                    <a href="https://www.instagram.com/mongamonga_" target="_blank">
+                        <i className="fa-brands fa-instagram icon"></i>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UC-MaHf-WV-C6yu99T61pk7A" target="_blank">
+                        <i className="fa-brands fa-youtube icon"></i>
+
+                    </a>
+                    <a href="https://www.tiktok.com/@mongamonga_?lang=en" target="_blank">
+                        <i className="fa-brands fa-tiktok icon tiktok"></i>
+                    </a>
+            </div>
+            <p><a>Royalty Free Lease</a></p>
+            <p className="copyright">©{getDate()} MongaMongaBeats LLC</p>
         </footer>
     )
 }
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import smokeableIcon from '../../assets/icons/logo-certified-smokeable.png'
 
 function Header() {
     return(
         <header className="header">
             <h1>mongamonga_</h1>
-            <p>Certified Smokeable</p>
+            <p className='header-text'>Certified Smokeable <span className='header-img-container'><img className='header-img' alt='Hand holding a blunt' src={smokeableIcon} /></span></p>
         </header>
     )
 }
