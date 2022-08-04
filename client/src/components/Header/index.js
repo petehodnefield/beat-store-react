4ed61f071d4f73f8a935e5d5f8ed3d69889e6126
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-08-04 09:29:56 -0500

    added mongerHoncho and Morie icon to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import smokeableIcon from '../../assets/icons/logo-certified-smokeable.png'
+import mongerHoncho from '../../assets/icons/mongerHoncho.png'
+import morie from '../../assets/icons/morie.png'
 
 function Header() {
     return(
         <header className="header">
             <h1>mongamonga_</h1>
             <p className='header-text'>Certified Smokeable <span className='header-img-container'><img className='header-img' alt='Hand holding a blunt' src={smokeableIcon} /></span></p>
+            <img
+                src={mongerHoncho}
+                alt="A cartoon gorilla"
+                className='honcho-icon'
+            />
+            <img
+                src={morie}
+                alt="A cartoon gorilla"
+                className='morie-icon'
+            />
         </header>
     )
 }
