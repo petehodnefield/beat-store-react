d1a8bd52a08c13e50be38cba723a525a8087be8e
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-08-04 10:35:47 -0500

    added form styling

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -15,15 +15,23 @@ const ContactForm = () => {
     return (
         <div className="container">
             <div className="form-container">
-                <h2 className="section-header">Custom Loops</h2>
+                <h2 className="section-header form-header">Custom Loops</h2>
                 <form onSubmit={(e) => formHandler(e)} className="contact-form">
-                    <input type="text" name="from_name" placeholder="Name"></input>
-                    <input type="email" name="user_email" placeholder="Email"></input>
-                    <input type="text" name="user_instagram" placeholder="Instagram handle"></input>
-                    <input type="url" name="reference_track" placeholder="Reference track"></input>
+                    <label className="label">Name</label>
+                    <input type="text" name="from_name" ></input>
+
+                    <label className="label">Email</label>
+                    <input type="email" name="user_email"></input>
+
+                    <label className="label">Instagram</label>
+                    <input type="text" name="user_instagram"></input>
+
+                    <label className="label">Reference Track (url)</label>
+                    <input type="url" name="reference_track" ></input>
+
                     <label className="label">Additional notes / requests:</label>
                     <textarea name="message" cols="30" rows="4"></textarea>
-                    <button className="btn submit-btn" type="submit">Submit</button>
+                    <button className="btn submit-btn" type="submit">SUBMIT</button>
                 </form>
             </div>
         </div>
