774e1aea89c5e36c65e2bbe73faea1c71e3d35ad
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-08-30 08:27:33 -0500

    replaced footer anchor with Link

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,33 +1,38 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import LoopLicense from "../LoopLicense";
 
-
-
 function Footer() {
+  const getDate = () => {
+    return new Date().toLocaleDateString("en-us", { year: "numeric" });
+  };
+  return (
+    <footer>
+      <div className="social">
+        <a href="https://www.instagram.com/mongamonga_" target="_blank">
+          <i className="fa-brands fa-instagram icon"></i>
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UC-MaHf-WV-C6yu99T61pk7A"
+          target="_blank"
+        >
+          <i className="fa-brands fa-youtube icon"></i>
+        </a>
+        <a href="https://www.tiktok.com/@mongamonga_?lang=en" target="_blank">
+          <i className="fa-brands fa-tiktok icon tiktok"></i>
+        </a>
+      </div>
+      <Link
+        className="license-link"
+        to="/loop-license"
+        element={<LoopLicense />}
+      >
+        Royalty Free License
+      </Link>
 
-    const getDate = () => {
-        return new Date().toLocaleDateString('en-us', {year: "numeric"})
-    }
-    return (
-        <footer>
-         
-           <div className="social">
-                    <a href="https://www.instagram.com/mongamonga_" target="_blank">
-                        <i className="fa-brands fa-instagram icon"></i>
-                    </a>
-                    <a href="https://www.youtube.com/channel/UC-MaHf-WV-C6yu99T61pk7A" target="_blank">
-                        <i className="fa-brands fa-youtube icon"></i>
-
-                    </a>
-                    <a href="https://www.tiktok.com/@mongamonga_?lang=en" target="_blank">
-                        <i className="fa-brands fa-tiktok icon tiktok"></i>
-                    </a>
-            </div>
-            <a className="license-link" href="http://localhost:3000/loop-license">Royalty Free License</a>
-            <p className="copyright">©{getDate()} MongaMongaBeats LLC</p>
-        </footer>
-    )
+      <p className="copyright">©{getDate()} MongaMongaBeats LLC</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
