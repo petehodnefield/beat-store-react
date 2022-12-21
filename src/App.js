c18ba4f8b9391772b9f77e75fc7d2f41dc6cc6f1
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-12-21 09:12:00 -0600

    deleted old content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React, { useEffect } from "react";
+import "./assets/styles/styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Header from "./components/Header";
-import Gallery from "./components/Gallery";
-import Home from "./pages/Home";
-import ContactForm from "./components/ContactForm";
-import Footer from "./components/Footer";
-import "./App.css";
-import "./styles.css";
-import LoopLicense from "./components/LoopLicense";
-import AirbitStore from "./components/AirbitStore";
-import FormSubmitted from "./pages/FormSubmitted";
-import ImageSlider from "./components/ImageSlider";
-import { SliderData } from "./components/SliderData";
-import NoMatch from "./pages/NoMatch";
-import FormWholePage from "./pages/FormWholePage";
 
 function App() {
   useEffect(() => {
@@ -23,20 +9,9 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header></Header>
         <div className="padding-bottom">
-          <Routes>
-            <Route path="/" element={<Home></Home>} />
-            <Route path="/beat-store-react/" element={<Home></Home>} />
-            <Route path="loop-license" element={<LoopLicense />} />
-            <Route path="airbit" element={<AirbitStore />} />
-            <Route path="/thank-you" element={<FormSubmitted />} />
-            <Route path="custom-loops" element={<FormWholePage />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
+          <Routes></Routes>
         </div>
-
-        <Footer></Footer>
       </div>
     </Router>
   );
